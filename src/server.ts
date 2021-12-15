a22dc6987f5162efe9c2c17eaceb617646efacf5
Sandro Roberto Pires <sandrorpires@gmail.com>
2021-12-15 15:42:26 -0300

    final projetct 1.0

--- src/server.ts
+++ src/server.ts
@@ -18,10 +18,8 @@ server.use(express.static(path.join(__dirname,'../public')));
 server.use(mainRoutes);
 
 server.use((req, res) => {
-    res.send('Página não encontrada!');
+    res.render('pages/404');
 });
-
-
 
 server.listen(process.env.PORT);
 //server.listen(4000);
